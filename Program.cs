95beeb6ee20bc2bc14c977d9c87abd38bb79549b
younz <younas1996@live.dk>
2025-01-08 15:31:23 +0100

    Update Neo4j connection and add repository services
    Updated Neo4j connection from local to remote instance in `Program.cs`.
    Replaced `bolt://localhost:7687` with `neo4j+s://736db64e.databases.neo4j.io`.
    Added repository services: `BarycenterRepositoryNEO4J`, `MoonRepositoryNEO4J`,
    `PlanetRepositoryNEO4J`, `MigrationServiceNEO4J`, and `ArtificialSatelliteRepositoryNEO4J`.

--- Program.cs
+++ Program.cs
@@ -68,7 +68,7 @@ builder.Services.AddScoped<ArtificialSatelliteRepositoryMySQL>();
 builder.Services.AddScoped<UserRepositoryMySQL>();
 
 // NEO4J
-builder.Services.AddSingleton(GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "Password123")));
+builder.Services.AddSingleton(GraphDatabase.Driver("neo4j+s://736db64e.databases.neo4j.io", AuthTokens.Basic("neo4j", "p6i9fPnQ8SCl4oxcccTrOMEosaHLdqeR-mPcklLCQTY")));
 builder.Services.AddScoped<BarycenterRepositoryNEO4J>();
 builder.Services.AddScoped<MoonRepositoryNEO4J>();
 builder.Services.AddScoped<PlanetRepositoryNEO4J>();
